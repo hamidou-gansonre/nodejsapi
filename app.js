35f0788308317c23b2e7f2b282489fb11e9a9e27
hamidou-gansonre <globyappouedra@gmail.com>
2024-06-03 14:30:06 +0000

    adding nodemon and morgan

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const router = express.Router();
 const contactRoutes = require('./api/routes/contacts');
 const todosRoutes = require('./api/routes/todos');
 const usersRoutes = require('./api/routes/users');
+const morgan = require('morgan');
+
+
+app.use(morgan('dev'));
 
 /**
  * Initialising Routes
@@ -14,6 +18,22 @@ const usersRoutes = require('./api/routes/users');
 app.use('/contacts', contactRoutes)
 app.use('/todos', todosRoutes);
 app.use('/users', usersRoutes);
+
+//Error handling
+app.use((req, res, next) => {
+    const error = new Error('Route not found');
+    error.status = 404 ;
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 //app.use(router);
 
