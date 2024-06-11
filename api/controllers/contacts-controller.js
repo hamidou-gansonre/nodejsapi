f49cbecba29d1440af8ea6241e61a7449e9c9a54
hamidou-gansonre <globyappouedra@gmail.com>
2024-06-11 12:07:27 +0000

    contact CRUD et final part for nodeApi

--- api/controllers/contacts-controller.js
+++ api/controllers/contacts-controller.js
@@ -1,46 +1,111 @@
 const Contact = require('../models/contact-model');
 class ContactsController {
+
+    //get all contacts
      getAll = () => {
-        return (req, res, next) => {
-            res.status(200).json({
-                success: true,
-                data: [
-                    {fname: "Hamidou", lname: "Gansonre", phone: "66236850"},
-                    {fname: "Hamid", lname: "Gansonre", phone: "68389819"},
-                
-                ]
-            });
+        return async (req, res, next) => {
+           const userId = req.userData.id;
+           const {count, rows} = await Contact.findAndCountAll({
+            where: {user_id: userId}
+           });
+           res.status(200).json({
+               success: true,
+               total: count,
+               data: rows
+           });
         }
      }
 
+//Create a new conatct
      create = () => {
-        return (req, res, next) => {
-         console.log(req.body)
-         const obj = {
-            fname: req.body.fname,
-            lname: req.body.lname,
-            email: req.body.email
+        return async (req, res, next) => {
+         try {
+            const userId = req.userData.id;
+            const conctact = await Contact.create({
+               fname: req.body.fname,
+               lname: req.body.lname,
+               email: req.body.email,
+               phone: req.body.phone,
+               user_id: userId
+            }) ;
+            res.status(201).json({success: true, contact: conctact});
+
+         } catch(err){
+             res.status(422).json(err.errors);
          }
-             res.status(200).json({success: true, method: "create", contact: obj}) ;
         }
      }
+
+     //find contact by id
 
      findById = () => {
-        return (req, res, next) => {
-         console.log(req.query)
-             res.status(200).json({success: true, method: "findById"}) ;
+        return async(req, res, next) => {
+            const userId = req.userData.id;
+            const contactId = req.params.id;
+            const contact = await Contact.findOne({
+               where: {
+                  id: contactId,
+                  user_id: userId
+               }
+            });
+            const resp = {success: false, contact: null};
+            if(contact){
+               resp.success = true;
+               resp.contact = contact;
+            }
+            res.status(200).json(resp);
         }
      }
 
+     //Update a contact
      update = () => {
-        return (req, res, next) => {
-             res.status(200).json({success: true, method: "update"}) ;
+        return async (req, res, next) => {
+         const userId = req.userData.id;
+         const contactId = req.params.id;
+         const resp = { success: false, contact: null, msg: "Contact not found" }; ;
+         const contact = await Contact.findOne({
+            where: {
+               id: contactId,
+               user_id: userId
+            }
+         }) ;
+         if(contact){
+            const vals = {
+               fname: req.body.fname,
+               lname: req.body.lname,
+               email: req.body.email,
+               phone: req.body.phone
+            } ;
+            await Contact.update(vals, {where: {id: contact.id}});
+            await contact.reload();
+            resp.success = true;
+            resp.msg = 'Contact updated successfully';
+            resp.contact = contact;
+         }
+         res.status(200).json(resp);
         }
      }
+
+     //Delete a contact
 
      delete = () => {
-        return (req, res, next) => {
-             res.status(200).json({success: true, method: "delete"}) ;
+        return async (req, res, next) => {
+         const userId = req.userData.id;
+         const contactId = req.params.id;
+         const contact = await Contact.findOne({
+            where: {
+               id: contactId,
+               user_id: userId
+            }
+         });
+         const resp = { success: false, contact: null, msg: "Contact not found" }; ;
+         if(contact){
+            await Contact.destroy({where: {id: contact.id}});
+            resp.success = true;
+            resp.msg = 'Contact deleted successfully';
+            resp.contact = contact;
+         }
+         res.status(200).json(resp);
         }
      }
 }
--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ContactsController = require('../controllers/contacts-controller');
+const userAuth = require("../middleware/check-auth");
 
-router.get('/', ContactsController.getAll());
-router.post('/', ContactsController.create());
+router.get('/', userAuth,  ContactsController.getAll());
+router.post('/', userAuth, ContactsController.create());
 
-router.get('/:id', ContactsController.findById());
-router.patch('/:id', ContactsController.update());
-router.delete('/:id', ContactsController.delete());
+router.get('/:id', userAuth, ContactsController.findById());
+router.patch('/:id', userAuth, ContactsController.update());
+router.delete('/:id', userAuth, ContactsController.delete());
 
 module.exports = router;
